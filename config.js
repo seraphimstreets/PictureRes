6dd51654fe8748c5ba2bde6f37d1029475ad2627
Edison Siow <edisonsiowxiong@gmail.com>
2020-09-02 08:10:04 +0800

    Webready 2

--- config.js
+++ config.js
@@ -4,28 +4,28 @@ module.exports = {
     'secretKey':'12345-67890-09876-54321',
     
     'facebook':{
-        clientId:"818166875257314",
-        clientSecret:"e0b2c57b41d95e106c7bc91a19fc2f50",
+        clientId:"FACEBOOK-CLIENT-ID",
+        clientSecret:"FACEBOOK-CLIENT-SECRET",
         callbackURL: baseUrl + '/users/auth/facebook/callback'
     },
     'google':{
-        clientID:"514434843520-p49lm90dlqo2ct0p7qe0oo0qfvhjue2e.apps.googleusercontent.com",
-        clientSecret:"YRaFYJTcqIwzv5vnn1_RH7pQ",
+        clientID:"GOOGLE-CLIENT-ID",
+        clientSecret:"GOOGLE-CLIENT-SECRET",
         callbackURL: baseUrl +'/users/auth/google/callback'
     },
     'tumblr':{
-        consumer_key: 'iDrl5D2b7dmfPpuPqytZElIYoeJobHEn7YPmrs4HOB1llT8Xul',
-        consumer_secret: 'aZqhD35feeMzPtTSv6sihX4UAEWYXUCIKtJMDUNWW6111eQaZX',
+        consumer_key: "TUMBLR-CLIENT-ID",
+        consumer_secret: "TUMBLR-CLIENT-SECRET",
         callbackURL: baseUrl + "/users/auth/tumblr/callback"
     },
     'github':{
-        clientID:"31c79fdef325e26efc0e",
-        clientSecret:"21cadfa33980395774d0433626dae433e475b381",
+        clientID:"GITHUB-CLIENT-ID",
+        clientSecret:"GITHUB-CLIENT-SECRET",
         callbackURL: baseUrl + "/users/auth/github/callback"
     },
     'twitter':{
-        consumerKey: "YSOFw6WXiILEkEyKWb1kt5jpj",
-        consumerSecret: "OaiUI8aGJ825PQwk59MzLyG0xQaV4Ra5vBbsAslzmGU1I3wCjX",
+        consumerKey:"TWITTER-CLIENT-ID",
+        consumerSecret: "TWITTER-CLIENT-SECRET",
         callbackURL: baseUrl + "/users/auth/twitter/callback"
 
     },
